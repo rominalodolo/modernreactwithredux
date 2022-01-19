551611af00d83c7e1dcfdb820706ab5d38c701f9
rominalodolo <lodoloromina@gmail.com>
2022-01-19 08:44:37 +0200

    update

--- greycross/src/index.js
+++ greycross/src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
+}
+ 
+class App extends React.Component {
+  state = {styleOne: {}, styleTwo: {} };
+  
+  onMouseMove = (event) => {
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY)
+    })
+  }
+ 
+  render() {
+    return <div onMouseMove={this.onMouseMove}>
+      <div className="panel" style={this.state.styleOne} />
+      <div className="panel" style={this.state.styleTwo} />      
+    </div>    
+  } 
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
